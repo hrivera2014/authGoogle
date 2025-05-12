0396c55ad11899341177cf4418df45a71b0b739e
Neo <hender123@yahoo.com>
2025-05-12 14:04:27 -0400

    user schema added

---
+++ user.schema.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+export const userSchema = new mongoose. Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: [true,"Email is required"],
+    match: [/^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/],
+  },
+  password: { type: String},
+  googleId: {type: String,},
+  accessToken: {type: String}
+})
