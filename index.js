b59f425fee76fc29dd5b6bf92e0cb7f30ae4b3f0
neo <harivera2014@gmail.com>
2025-05-13 11:14:18 -0400

    original scanned text enhanced advances

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import dotenv from "dotenv";
-dotenv.config();
+//import dotenv from "dotenv";
+//dotenv.config();
 import cookieParser from "cookie-parser"
 import express from "express"
-import ejsLayouts from "express-ejs-layouts";
-import path from "path";
-import passport from "passport";
-import session from "express-session";
-import jwt from "jsonwebtoken";
-import {googlePassportConfig} from "./src/config/passport-google.js";
-import userRouter from "./src/features/user/user.routes.js";
+i//import ejsLayouts from "express-ejs-layouts";
+import path from "node:path"
+import passport from "passport"
+import session from "express-session"
+import jwt from "jsonwebtoken"
+import { googlePassportConfig } from "./src/config/passport-google.js"
+import userRouter from "./src/features/user/user.routes.js"
 import otpRouter from "./src/features/otp/otp.routes.js"
 const app = express();
 
@@ -16,14 +16,15 @@ app.set("view engine","ejs");
 app.set("views",path.join(path.resolve(),"src", "views"));
 
 // for below to work have a layout.ejs in views
-app.use(ejsLayouts);
+//app.use(ejsLayouts);
 
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  //secret: process.env.SESSION_SECRET,
+  secret: "secret",
   resave: false,
   saveUninitialized: true,
 }));
-app.use(express.urlencoded({extended:truc}));
+app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 googlePassportConfig(passport);
 app.get("/", (req,res)=>{
@@ -40,14 +41,15 @@ app.get("/auth/google/callback",
 passport.authenticate("google", { failureRedirect: "/login" }),
 (req, res) => {
 //see the structure of req object to understaand better
-const payload = {_id: req.user._id, email: req.user.email,accessToken:req.user.accessTc
-jwt.sign(payload, process.env.JNT_SECRET, { expiresIn: "1h" }, (err, token) => {
-if (err) throw err;
-res.cookie(jutToken, token, { httpOnly: true });
-res.redirect("/user/home");
-}
-}
-)
+const payload = {_id: req.user._id, email: req.user.email,accessToken:req.user.accessToken
+//jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" }, (err, token) => {
+jwt.sign(payload, "secret", { expiresIn: "1h" }, (err, token) => {
+	if (err) throw err;
+	res.cookie(jutToken, token, { httpOnly: true });
+	res.redirect("/user/home");
+	}
+})
 app.use("/user", userRouter);
 app.use("/otp",otpRouter);
+
 export default app;
